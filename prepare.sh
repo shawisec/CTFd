18a17eb6b4e712d110220775cb4dd907c29dc24a
mstyves <mathieu.st-yves@hotmail.com>
2019-01-19 13:57:33 -0500

    test d'importation bd

--- prepare.sh
+++ prepare.sh
@@ -5,9 +5,9 @@ pip install -r requirements.txt
 
 # Initialize database
 python manage.py db upgrade
-if [ ! -f shawisec.zip ] ; then
+if [ ! -f logs/shawisec.zip ] ; then
   echo "NO data imported"
 else
-  python import.py shawisec.zip challenges
-  rm -f shawisec.zip #Importation une seule fois
+  python import.py logs/shawisec.zip challenges
+  rm -f logs/shawisec.zip #Importation une seule fois
 fi
