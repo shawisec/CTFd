21c07b01632ee2801f4c1ce9062e82af42236bfa
mstyves <mathieu.st-yves@hotmail.com>
2019-01-19 14:02:00 -0500

    remove auto load challenge

--- prepare.sh
+++ prepare.sh
@@ -2,12 +2,3 @@
 sudo apt-get update
 sudo apt-get install build-essential python-dev python-pip libffi-dev -y
 pip install -r requirements.txt
-
-# Initialize database
-python manage.py db upgrade
-if [ ! -f logs/shawisec.zip ] ; then
-  echo "NO data imported"
-else
-  python import.py logs/shawisec.zip challenges
-  rm -f logs/shawisec.zip #Importation une seule fois
-fi
